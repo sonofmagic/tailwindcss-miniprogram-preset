74270f446b8b5555b872ee0d84fdfa02b28f0eed
SonOfMagic <qq1324318532@gmail.com>
2021-08-29 15:56:12 +0800

    feat: native example remove *.js

--- examples/native/miniprogram/pages/index/index.ts
+++ examples/native/miniprogram/pages/index/index.ts
@@ -4,17 +4,19 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'icebreaker',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData:
+      wx.canIUse('open-data.type.userAvatarUrl') &&
+      wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
-      url: '../logs/logs',
+      url: '../logs/logs'
     })
   },
   onLoad() {
